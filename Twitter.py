e903e42dc0a616e2b08fef6257cff422834f6c96
ppoguntke <83464356+ppoguntke@users.noreply.github.com>
2021-05-01 20:07:04 +0200

    some small improvements

--- Twitter.py
+++ Twitter.py
@@ -5,12 +5,13 @@ import pandas as pd
 tweets_list1 = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:BillGates since:2019-05-01').get_items()):
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:JeffBezos since:2019-05-01').get_items()):
     if i>500:
         break
     tweets_list1.append([tweet.date, tweet.id, tweet.content, tweet.username])
     
 # Creating a dataframe from the tweets list above 
 tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+tweets_df1['Text'] = tweets_df1['Text'].replace({'â€™':"'"})
 
-tweets_df1.to_csv (r'C:\Users\patri\Desktop\HEC\5th Bimester\Business Analytics\bill_gates.csv')
+tweets_df1.to_csv (r'C:\Users\patri\Desktop\HEC\5th Bimester\Business Analytics\jeff_bezos.csv', encoding='utf-8-sig')
