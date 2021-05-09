9eae1267ef42d821179942127f5a5556c4353d88
Tom Eberle <eberle.tom@gmail.com>
2021-05-09 14:45:14 +0200

    Small edits on Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -20,20 +20,32 @@ def find_companies(content):
     """ Identifies stock market companies in scrapped content. 
     Input: [[counter, date, _time, title, author, likes, comments], [...]]
     Output: identical list with new 'company' column. 
+    Notes: removes the 1 line of the list (header)
     """
     nasdaq = load_csv(filename='nasdaq-listed_clean', subfolder='assets')
     test_string = "This text is about a company called credit suisse company and , of couse, is it working"
 
+    # Removing headers
+    content.pop(0)
+
     # TODO: for loop to check if company is in: title, content, etc.
-    for company in nasdaq:
-        match_name = test_string.find(company[1])
-        if match_name != -1:
-            print('Found company: ' + str(company))
+    for row in content:
+        # Input: [counter,date,_time,title,author,likes,comments,url,content]
+        tickers = ''
+        title = row[3]
+        content = row[8]
 
-        match_symbol = test_string.find(company[0])
-        if match_symbol != -1:
-            print('Found company: ' + str(company))
+        for company in nasdaq:
+            # Looking company name
+            match_name = title.find(company[1])
+            if match_name != -1:
+                print(row[0] + '- Found company in title: ' + str(company))
+            # Looking ticker
+            match_symbol = title.find('$' + company[0])
+            if match_symbol != -1:
+                print(row[0] + '- Found ticker in title: ' + str(company))
 
+    # TODO: Better matching accuracy
     # TODO: create new column with tickers
 
 
