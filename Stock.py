41e9421384c3ed7619a0c2566dc2a325c19688e4
Tom Eberle <eberle.tom@gmail.com>
2021-05-11 13:36:15 +0200

    Stock data API

--- Stock.py
+++ Stock.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def get_stock_data(ticker_list, start_date, end_date, interval):
-    data = yf.download(
+    """ data = yf.download(
         tickers=ticker_list,
         start=start_date,
         end=end_date,
@@ -17,10 +17,15 @@ def get_stock_data(ticker_list, start_date, end_date, interval):
         # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
         # (optional, default is '1d')
         interval=interval)
+        """
+    # data = yf.download('AAPL MC.PA', '2021-01-01', '2021-08-01', interval="1h")
+    data = yf.download(ticker_list, start_date, end_date, interval=interval)
     # Plot the close prices
+    print(data)
     data.Close.plot()
     plt.show()
 
 
-get_stock_data(ticker_list='MSFT', start_date="2021-04-10",
-               end_date="2021-04-10", interval='5m')
+# Example usage
+get_stock_data(ticker_list="AAPL MC.PA", start_date="2021-01-01",
+               end_date="2021-08-01", interval="1h")
