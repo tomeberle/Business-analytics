f03324718df6bf46eead30627c20e83587696471
ppoguntke <83464356+ppoguntke@users.noreply.github.com>
2021-05-19 14:28:59 +0200

    added company code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from Stock import get_stock_data_2min_56days
 from Twitter_CEO import get_CEOs_twitter_posts
-
+from Twitter_Company import get_company_twitter_posts
 
 # Load list of companies
 companies = pd.read_csv("assets/twitter_accounts.csv")
@@ -12,10 +12,13 @@ symbols = companies["symbol"].tolist()
 get_stock_data_2min_56days(symbols)
 
 # Fetch Twitter Company Posts
-get_CEOs_twitter_posts(companies)
+get_company_twitter_posts(companies)
+
 # Add Stock Price Movement in new column
 
 # Fetch Twitter CEO Posts
+get_CEOs_twitter_posts(companies)
+
 # Add Stock Price Movement in new column
 
 # Statistical tests Company Posts & CEO Posts
