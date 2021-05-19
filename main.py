050fccab26387e90ea490280ca1c4ad3e2d15a8d
Tom Eberle <eberle.tom@gmail.com>
2021-05-19 13:40:48 +0200

    Todo

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from Stock import get_stock_data_2min_56days
 
 # Load list of companies
 companies = pd.read_csv("assets/twitter_accounts.csv")
-companies = companies["symbol"].tolist()
+symbols = companies["symbol"].tolist()
 
 # Fetch stock data
-get_stock_data_2min_56days(companies)
+get_stock_data_2min_56days(symbols)
+
+# Fetch Twitter Company Posts
+# Add Stock Price Movement in new column
+
+# Fetch Twitter CEO Posts
+# Add Stock Price Movement in new column
+
+# Statistical tests Company Posts & CEO Posts
