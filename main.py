66782268ff90e8055b0eb6107977722e1865b8b1
Tom Eberle <eberle.tom@gmail.com>
2021-05-19 19:05:01 +0200

    Cleaning

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ stock_prices = get_stock_data_2min_56days(symbols)
 
 # Fetch Twitter CEO Posts
 CEO_tweets = get_CEOs_twitter_posts(companies)
-
-print(CEO_tweets)
-
-# Add Stock Price Movement in new column
-# CEO_tweets = CEO_tweets.assign(stock_mouvement=lambda x: find_stock_movement(
-#    x.ticker, x.date, time_after_tweet=8, time_before_tweet=2, sensitivity=0.01))
-#
-print(CEO_tweets)
-
 # Fetch Twitter Company Posts
 company_tweets = get_company_twitter_posts(companies)
-print(company_tweets)
 
 # Add Stock Price Movement in new column
+CEO_tweets['Movement'] = CEO_tweets.apply(lambda x: find_stock_movement(
+    x['Symbol'], x['Datetime'], time_after_tweet=10, time_before_tweet=4, sensitivity=0.005, df=stock_prices), axis=1)
+company_tweets['Movement'] = company_tweets.apply(lambda x: find_stock_movement(
+    x['Symbol'], x['Datetime'], time_after_tweet=10, time_before_tweet=4, sensitivity=0.005, df=stock_prices), axis=1)
+
+CEO_tweets.to_csv("output/twitter_sentiment_ceos.csv")
+company_tweets.to_csv("output/twitter_sentiment_companies.csv")
+
+print(CEO_tweets)
+print(company_tweets)
 
 
 # Statistical tests Company Posts & CEO Posts
