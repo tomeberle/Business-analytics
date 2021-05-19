e27989faf747296e7d9ce0ebf8f798271d4a1df3
Tom Eberle <eberle.tom@gmail.com>
2021-05-19 16:46:57 +0200

    Updating Twitter_CEO with symbol

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from Stock import get_stock_data_2min_56days
 from Twitter_CEO import get_CEOs_twitter_posts
 from Twitter_Company import get_company_twitter_posts
+from Analysis import find_stock_movement
 
 # Load list of companies
 companies = pd.read_csv("assets/twitter_accounts.csv")
@@ -13,10 +14,14 @@ stock_prices = get_stock_data_2min_56days(symbols)
 
 # Fetch Twitter CEO Posts
 CEO_tweets = get_CEOs_twitter_posts(companies)
-# Add Stock Price Movement in new column
 
 print(CEO_tweets)
 
+# Add Stock Price Movement in new column
+# CEO_tweets = CEO_tweets.assign(stock_mouvement=lambda x: find_stock_movement(
+#    x.ticker, x.date, time_after_tweet=8, time_before_tweet=2, sensitivity=0.01))
+#
+print(CEO_tweets)
 
 # Fetch Twitter Company Posts
 company_tweets = get_company_twitter_posts(companies)
