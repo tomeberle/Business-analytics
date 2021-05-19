1e3d477f353dc36acc7442f369179805dc3962fc
ppoguntke <83464356+ppoguntke@users.noreply.github.com>
2021-05-19 19:45:51 +0200

    some cleaning

--- statistical_tests.py
+++ statistical_tests.py
@@ -3,27 +3,33 @@ from scipy.stats import chi2_contingency
 
 def run_chisquared_company(tweets_df1):
     tweets_df1 = pd.read_csv("output/twitter_sentiment_companies.csv")
-    
+    tweets_df1.dropna(subset=["Movement"], inplace=True)
+
     tweets_df1.loc[(tweets_df1.Sentiment > 0),'Sentiment']= 1
     tweets_df1.loc[(tweets_df1.Sentiment < 0),'Sentiment']= -1
     tweets_df1['Sentiment'] = tweets_df1['Sentiment'].replace([0, 1, -1],['neutral','positive','negative'])
 
+    tweets_df1['Movement'] = tweets_df1['Movement'].astype(int)
     tweets_df1['Movement'] = tweets_df1['Movement'].replace([0, 1, -1],['no_move','up','down'])
+    print(tweets_df1)
 
     # Contingency Table
     contingency = pd.crosstab(tweets_df1['Sentiment'], tweets_df1['Movement'])
 
     # Chi-Squared Test
     chi2, p, dof, expected = chi2_contingency(contingency)
-    chi2, p
+    return chi2, p
 
 
 def run_gtest_company(tweets_df1):
     tweets_df1 = pd.read_csv("output/twitter_sentiment_companies.csv")
+    tweets_df1.dropna(subset=["Movement"], inplace=True)
+    
     tweets_df1.loc[(tweets_df1.Sentiment > 0),'Sentiment']= 1
     tweets_df1.loc[(tweets_df1.Sentiment < 0),'Sentiment']= -1
     tweets_df1['Sentiment'] = tweets_df1['Sentiment'].replace([0, 1, -1],['neutral','positive','negative'])
 
+    tweets_df1['Movement'] = tweets_df1['Movement'].astype(int)
     tweets_df1['Movement'] = tweets_df1['Movement'].replace([0, 1, -1],['no_move','up','down'])
 
     # Contingency Table
@@ -31,16 +37,18 @@ def run_gtest_company(tweets_df1):
 
     # G-Test or log-likelihood ratio
     g, p, dof, expcted = chi2_contingency(contingency, lambda_="log-likelihood")
-    g, p
+    return g, p
 
 
 def run_chisquared_ceo(tweets_df1):
     tweets_df1 = pd.read_csv("output/twitter_sentiment_ceos.csv")
+    tweets_df1.dropna(subset=["Movement"], inplace=True)
     
     tweets_df1.loc[(tweets_df1.Sentiment > 0),'Sentiment']= 1
     tweets_df1.loc[(tweets_df1.Sentiment < 0),'Sentiment']= -1
     tweets_df1['Sentiment'] = tweets_df1['Sentiment'].replace([0, 1, -1],['neutral','positive','negative'])
-
+    
+    tweets_df1['Movement'] = tweets_df1['Movement'].astype(int)
     tweets_df1['Movement'] = tweets_df1['Movement'].replace([0, 1, -1],['no_move','up','down'])
 
     # Contingency Table
@@ -48,15 +56,18 @@ def run_chisquared_ceo(tweets_df1):
 
     # Chi-Squared Test
     chi2, p, dof, expected = chi2_contingency(contingency)
-    chi2, p
+    return chi2, p
 
 
 def run_gtest_ceo(tweets_df1):
     tweets_df1 = pd.read_csv("output/twitter_sentiment_companies.csv")
+    tweets_df1.dropna(subset=["Movement"], inplace=True)
+    
     tweets_df1.loc[(tweets_df1.Sentiment > 0),'Sentiment']= 1
     tweets_df1.loc[(tweets_df1.Sentiment < 0),'Sentiment']= -1
     tweets_df1['Sentiment'] = tweets_df1['Sentiment'].replace([0, 1, -1],['neutral','positive','negative'])
 
+    tweets_df1['Movement'] = tweets_df1['Movement'].astype(int)
     tweets_df1['Movement'] = tweets_df1['Movement'].replace([0, 1, -1],['no_move','up','down'])
 
     # Contingency Table
@@ -64,4 +75,4 @@ def run_gtest_ceo(tweets_df1):
 
     # G-Test or log-likelihood ratio
     g, p, dof, expcted = chi2_contingency(contingency, lambda_="log-likelihood")
-    g, p
+    return g, p
